26d7ac6f2afe8f1315a6e2e9ba75d2b67d8bd692
George Hotelling <george.hotelling@automattic.com>
2018-12-06 11:14:19 -0500

    Day 5 now runs in 1/2s instead of 30s

--- 2018/day05.js
+++ 2018/day05.js
@@ -7,44 +7,38 @@ input = input.split( '' );
 const reacts = ( a, b ) => ( a !== b && a.toUpperCase() == b.toUpperCase() );
 
 function nextUnitIndex( polymer, idx ) {
-	for ( i = idx; i < polymer.length; i++ ) {
+	for ( let i = idx; i < polymer.length; i++ ) {
 		if ( '#' !== polymer[i] ) {
 			return i;
 		}
 	}
+	return -1;
+}
+
+function prevUnitIndex( polymer, idx ) {
+	for ( let i = idx; i > 0; i-- ) {
+		if ( '#' !== polymer[i] ) {
+			return i;
+		}
+	}
+	return 0;
 }
 
 function react( polymer ) {
-	let reacting = true;
 	let newPolymer = polymer.slice();
-	while ( reacting ) {
-		reacting = false;
-		// Array.slice() is slowwwww, sped this up from 1m30s to 0m30s by not making new arrays
-		// for ( i = 0; i < newPolymer.length - 1; i++ ) {
-		// 	if ( reacts( newPolymer[ i ], newPolymer[ i + 1 ] ) ) {
-		// 		newPolymer = newPolymer.slice( 0, i ).concat( newPolymer.slice( i + 2 ) );
-		// 		reacting = true;
-		// 	}
-		// }
-		for ( i = 0; i < newPolymer.length; i++ ) {
-			let unitAidx = nextUnitIndex( newPolymer, i ),
-				unitBidx = nextUnitIndex( newPolymer, unitAidx + 1 );
-
-			// console.log( i, unitAidx, unitBidx );
-
-			if ( ! unitBidx ) {
-				break;
-			}
-
-			if ( reacts( newPolymer[ unitAidx ], newPolymer[ unitBidx ] ) ) {
-				newPolymer[ unitAidx ] = '#';
-				newPolymer[ unitBidx ] = '#';
-				reacting = true;
-			}
-			i = unitBidx - 1;
+	let i = 0, j = 1;
+	while ( i != -1 && j != -1 ) {
+		if ( reacts( newPolymer[ i ], newPolymer[ j ] ) ) {
+			newPolymer[ i ] = '#';
+			newPolymer[ j ] = '#';
+			j = nextUnitIndex( newPolymer, j + 1 );
+			i = prevUnitIndex( newPolymer, i - 1 );
+		} else {
+			i = nextUnitIndex( newPolymer, i + 1 );
+			j = nextUnitIndex( newPolymer, i + 1 );
 		}
-		// console.log( newPolymer.join( '' ), reacting );
 	}
+
 	return newPolymer.filter( u => u !== '#' );
 }
 
@@ -54,15 +48,15 @@ const part1 = react( input );
 console.log( 'Part 1:', part1.length );
 
 function uniqueUnits( polymer ) {
-	const seen = new Map();
-	polymer.forEach( u => seen.set( u.toUpperCase(), true) );
+	const seen = new Set();
+	polymer.forEach( u => seen.add( u.toUpperCase() ) );
 	return Array.from( seen.keys() );
 }
 
 const removeUnits = ( p ) => ( u ) => p.filter( l => l.toUpperCase() != u );
 
 console.log( 'Part 2:',
-	uniqueUnits( input )
+	uniqueUnits( part1 )
 	.map( removeUnits( input ) )
 	.map( react )
 	.map( p => p.length )
